3234af2e252ea9ecbe0fabc0e62b140ff007b423
chend <chend@users.noreply.github.com>
2026-06-09 15:04:08 +0800

    assets: 22 film frame overlays (4208x3120 RGBA)

---
+++ android/build.gradle.kts
@@ -0,0 +1,21 @@
+allprojects {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.buildDir = file("../build")
+subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
