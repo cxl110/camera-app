ef7aa3ae52b723d1eae176407823da14f02d03a7
chend <chend@users.noreply.github.com>
2026-07-20 13:01:22 +0800

    fix: home_screen 缺少 HttpCameraProtocol import

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../services/camera_protocol.dart';
+import '../services/http_camera_protocol.dart';
 import '../services/image_service.dart';
 import '../widgets/wifi_indicator.dart';
 import '../widgets/camera_preview.dart';
